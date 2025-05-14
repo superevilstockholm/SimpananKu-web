c4ea0f4884eb0921431fdfa2c672e1ac4b99a173
superevilstockholm <balilombok11@gmail.com>
2025-05-14 23:21:24 +0700

    added sweetalert to login and register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,20 +12,32 @@ class AuthController extends Controller
     public function Login(Request $request) {
         if ($request->has('nisn')) {
             // Login siswa
+            $validate = $request->validate([
+                'nisn' => 'required|digits:10',
+                'password' => 'required|min:8|max:64'
+            ]);
+            return response()->json(['status' => true, 'message' => 'Berhasil masuk sebagai siswa']);
         } else if ($request->has('nik')) {
             // Login guru
+            $validate = $request->validate([
+                'nik' => 'required|digits:16',
+                'password' => 'required|min:8|max:64'
+            ]);
+            return response()->json(['status' => true, 'message' => 'Berhasil masuk sebagai guru']);
         } else {
-            return response()->json(['status'=> false, 'message' => 'Login Failed. Invalid Credentials'], 401);
+            return response()->json(['status'=> false, 'message' => 'kredensial yang tidak valid'], 401);
         }
     }
 
     public function Register(Request $request) {
         if ($request->has('nisn')) {
             // Daftar siswa
+            return response()->json(['status' => true, 'message' => 'Berhasil mendaftar sebagai siswa']);
         } else if ($request->has('nik')) {
             // Daftar guru
+            return response()->json(['status' => true, 'message' => 'Berhasil mendaftar sebagai guru']);
         } else {
-            return response()->json(['status'=> false, 'message' => 'Login Failed. Invalid Credentials'], 401);
+            return response()->json(['status'=> false, 'message' => 'kredensial yang tidak valid'], 401);
         }
     }
 }
