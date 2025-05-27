f6f82c0d70040b7bd57dc9250d0662e94c16e56d
nainayy992 <azahrakeylaa45@example.com>
2025-05-27 11:55:37 +0700

    ganti bg

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,14 @@
 @extends('App') @section('title', 'About - SimpananKu')
 @section('content')
-<div class="container py-5">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <div class="bg-dark p-5 shadow rounded-4">
+<div class="position-relative px-0 py-5 m-0">
+    <div class="position-absolute w-100 h-100 p-0 m-0 z-n1 d-md-block d-none">
+        <div class="container w-100 h-100 py-0 my-0">
+            <div class="w-100 h-100 p-0 m-0 ratio ratio-1x1" style="animation: fade-in 1s ease;">
+                <div class="rounded-circle bg-primary w-100 w-md-75 w-lg-80 top-100 start-0" style="filter: blur(100px); transform: translate(-50%, -50%); opacity: 0.7;"></div>
+                <div class="rounded-circle bg-primary w-100 w-md-75 w-lg-80 top-0 start-100" style="filter: blur(100px); transform: translate(-50%, -50%); opacity: 0.7;"></div>
+            </div>
+        </div>
+    </div>
                 <h1 class="mb-4 text-center fw-bold text-primary">Tentang SimpananKu</h1>
                 <p class="lead">
                     <strong>SimpananKu</strong> Website SimpananKu memudahkan siswa, orang tua, dan guru dalam mengelola dan memantau tabungan siswa. 
@@ -19,10 +24,7 @@
                 <div class="text-center mt-4">
                     <a href="{{ url('/') }}" class="btn btn-primary px-4">Kembali ke Beranda</a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 @endsection
 
 
