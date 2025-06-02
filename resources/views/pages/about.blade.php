420aab689a4dd6283a6f4979ff6c6f4d5f9e116f
nainayy992 <azahrakeylaa45@example.com>
2025-06-02 12:03:37 +0700

    coba desk tidak rata tengah dan merapihkan barisan nya

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -15,13 +15,13 @@
     <div class="max-w-6xl mx-auto mt-10 px-4">
         <div class="container py-5 my-3 text-center">
             <h1 class="mb-5 fw-bold">Tentang SimpananKu</h1>
-            <p class="text-gray-500">
+            <p class="text-gray-500 mb-5">
                 Ketahui lebih banyak tentang tim yang memelihara SimpananKu, bagaimana dan mengapa proyek ini dimulai, dan bagaimana cara terlibat.
             </p>
         </div>
         <section class="text-center mb-5">
             <h2 class="text-2xl font-bold mb-2">Team</h2>
-            <p class="text-gray-500">
+            <p class="text-gray-500 mb-5">
                 Simpananku dikelola oleh tim kecil pengembang di SMKN 4. Kami sedang aktif bekerja untuk mengembangkan tim ini dan akan senang melihat serta mendengar dari Anda jika Anda tertarik dalam menabung secara online, keamanan yang terjaga.
             </p>
         </section>
@@ -30,13 +30,12 @@
             <p class="text-gray-500">
                 SimpananKu dibuat oleh seorang desainer dan pengembang dari Departemen RPL SMKN 4, SimpananKu menjadi salah satu sumber untuk menabung online yang dikembangkan oleh siswa/i XI RPL 3.
                 <br><br>
-                SimpananKu was created in 2025 by @fakhri1104, @bjorki199, @a1fkhriii_, @nskylzhra, @jeksnopcat41_ .
+                SimpananKu was created in 2025 by @fakhri1104, @bjorki199, @a1fkhriii_, @nskylzhra, @jeksnopcat41_.
             </p>
         </section>
-            <div class="text-center mt-8">
-                <a href="{{ url('/') }}" class="btn btn-primary px-4">Kembali ke Beranda</a>
-            </div>
-        </section>
+        <div class="text-center mt-8">
+            <a href="{{ url('/') }}" class="btn btn-primary px-4">Kembali ke Beranda</a>
+        </div>
     </div>
 </div>
 @endsection
