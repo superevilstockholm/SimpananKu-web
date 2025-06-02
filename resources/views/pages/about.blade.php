da731c9253b2af32ff2ed0116528cd1531c6707c
nainayy992 <azahrakeylaa45@example.com>
2025-06-02 11:58:15 +0700

    update desk team dan menghapus bagian developer list

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -20,51 +20,19 @@
             </p>
         </div>
         <section class="text-center mb-5">
-            <h2 class="text-2xl font-bold mb-2">History</h2>
+            <h2 class="text-2xl font-bold mb-2">Team</h2>
             <p class="text-gray-500">
-                SimpananKu dibuat oleh seorang desainer dan pengembang dari Departemen RPL SMKN 4. SimpananKu menjadi salah satu sumber untuk menabung online yang dikembangkan oleh siswa/i XI RPL 3.
-                <br><br>
-                SimpananKu was created in 2025 by @fkhri, @bjrki, @alfkhr, @nai, @dzki.
+                Simpananku dikelola oleh tim kecil pengembang di SMKN 4. Kami sedang aktif bekerja untuk mengembangkan tim ini dan akan senang melihat serta mendengar dari Anda jika Anda tertarik dalam menabung secara online, keamanan yang terjaga.
             </p>
         </section>
-
-        <section class="mx-auto px-6 py-12">
-            <h2 class="text-3xl font-bold text-center mb-12">Developer List</h2>
-
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 text-center">
-                <!-- Developer Cards -->
-                <div class="bg-white border rounded shadow p-4">
-                    <img src="{{ asset('fakhri.png') }}" class="mx-auto w-24 h-24 object-cover rounded-full" />
-                    <h3 class="mt-2 font-semibold">Muhammad Fakhri</h3>
-                    <p>( Leader )</p>
-                    <p class="font-bold">As BackEnd</p>
-                </div>
-
-                <div class="bg-white border rounded shadow p-4">
-                    <img src="{{ asset('suki.png') }}" class="mx-auto w-24 h-24 object-cover rounded-full" />
-                    <h3 class="mt-2 font-semibold">Rizky Aditia Zaenal</h3>
-                    <p class="font-bold">Fullstack</p>
-                </div>
-
-                <div class="bg-white border rounded shadow p-4">
-                    <img src="{{ asset('nais.png') }}" class="mx-auto w-24 h-24 object-cover rounded-full" />
-                    <h3 class="mt-2 font-semibold">Naisya Keyla Azahra</h3>
-                    <p class="font-bold">As Designer & FrontEnd</p>
-                </div>
-
-                <div class="bg-white border rounded shadow p-4">
-                    <img src="{{ asset('alfkhr.png') }}" class="mx-auto w-24 h-24 object-cover rounded-full" />
-                    <h3 class="mt-2 font-semibold">Ahmad Al-Fakhri Maulana</h3>
-                    <p class="font-bold">As FrontEnd</p>
-                </div>
-
-                <div class="bg-white border rounded shadow p-4">
-                    <img src="{{ asset('dzki.png') }}" class="mx-auto w-24 h-24 object-cover rounded-full" />
-                    <h3 class="mt-2 font-semibold">Dzaki Saputra</h3>
-                    <p class="font-bold">As Designer</p>
-                </div>
-            </div>
-
+        <section class="text-center mb-5">
+            <h2 class="text-2xl font-bold mb-2">History</h2>
+            <p class="text-gray-500">
+                SimpananKu dibuat oleh seorang desainer dan pengembang dari Departemen RPL SMKN 4, SimpananKu menjadi salah satu sumber untuk menabung online yang dikembangkan oleh siswa/i XI RPL 3.
+                <br><br>
+                SimpananKu was created in 2025 by @fakhri1104, @bjorki199, @a1fkhriii_, @nskylzhra, @jeksnopcat41_ .
+            </p>
+        </section>
             <div class="text-center mt-8">
                 <a href="{{ url('/') }}" class="btn btn-primary px-4">Kembali ke Beranda</a>
             </div>
